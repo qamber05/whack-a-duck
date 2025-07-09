9eafbc0523d9913d6ce9cbadb12139e14c60e3ea
qamber05 <mohammedqamber11@gmail.com>
2025-07-09 11:41:14 +0300

    add duck class

--- whack-a-duck/app.js
+++ whack-a-duck/app.js
@@ -1,17 +1,24 @@
 function init() {
 //all code goes here
 const gridElem = document.querySelector('.grid')
+
+const cells = []
+const gridwidth = 10
+const numberOfCells = gridwidth * gridwidth
+
 function createGrid(){
     //for every cell wer require create a grid
     //then appen this cell to our grid
 
-    for (let i=0;i<100;i++){
+    for (let i=0;i<numberOfCells;i++){
         const cell=document.createElement('div')
+        cell.classList.add('duck')
         cell.textContent= i
+        cells.push(cell)  
         gridElem.appendChild(cell)
 
     }
-   
+   console.log(cells)
 }
  createGrid()
 }
