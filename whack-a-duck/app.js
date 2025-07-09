b76dda6fdd843e391bc7f40a66b1be2ce5edf9a5
qamber05 <mohammedqamber11@gmail.com>
2025-07-09 15:02:14 +0300

    finished

--- whack-a-duck/app.js
+++ whack-a-duck/app.js
@@ -2,6 +2,7 @@ function init() {
 //all code goes here
 const gridElem = document.querySelector('.grid')
 const scoreElem = document.querySelector('#score-display')
+const audioElem = document.querySelector('#sound')
 
 const cells = []
 const gridwidth = 10
@@ -10,9 +11,12 @@ const numberOfCells = gridwidth * gridwidth
 let duckPosition = 50
 
 let score = 0 
+scoreElem.textContent = `YOUR SCORE IS 0`
+let totalDucks = 0
 
 function addDuck() {
     cells[duckPosition].classList.add('duck')
+    totalDucks++
 }
 
 function removeDuck() {
@@ -20,14 +24,26 @@ function removeDuck() {
 
 }
 
+function endgame() {
+    alert('game ended, score: ' + score)
+    score = 0
+    totalDucks = 0
+}
+
 function play() {
 //    setInterval(functiom() {
 
   //  })
   setInterval(()=> {
-    removeDuck()
+    if (totalDucks < 10 ){ 
+        removeDuck()
     duckPosition=Math.floor(Math.random() * numberOfCells)
     addDuck()  
+    } else {
+        endgame()
+      
+    }
+    
 
   }, 3000 /* milisecs */)
 }
@@ -35,8 +51,11 @@ function play() {
 function handleClick(event) {
     console.log('handle click has run!')
     if (event.target.classList.contains('duck')) {
+        audioElem.pause()
+        audioElem.currentTime = 0
         score +=10 
-        scoreElem.textContent= `your score is ${score}`  
+        scoreElem.textContent= `your score is ${score}`
+        audioElem.play()   
         console.log(score)
     }
 }
