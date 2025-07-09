fb3ed6af8935bdd280ead719733fad8615b096f6
qamber05 <mohammedqamber11@gmail.com>
2025-07-09 11:14:34 +0300

    create grid

--- whack-a-duck/app.js
+++ whack-a-duck/app.js
@@ -1,4 +1,18 @@
 function init() {
+//all code goes here
+const gridElem = document.querySelector('.grid')
+function createGrid(){
+    //for every cell wer require create a grid
+    //then appen this cell to our grid
 
+    for (let i=0;i<100;i++){
+        const cell=document.createElement('div')
+        cell.textContent= i
+        gridElem.appendChild(cell)
+
+    }
+   
+}
+ createGrid()
 }
 document.addEventListener('DOMContentLoaded',init)
